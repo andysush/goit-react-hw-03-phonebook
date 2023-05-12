b25cd7d9e40870158a09b00ffebcf176f55c9e7e
Andrii <sushkoandy1@gmail.com>
2023-05-12 15:23:38 +0300

    add LocalStorage save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,17 @@ import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
+  };
+  componentDidMount = () => {
+    const storageData = localStorage.getItem('contact');
+    if (storageData) this.setState({ contacts: JSON.parse(storageData) });
+  };
+
+  componentDidUpdate = (_, prevState) => {
+    if (prevState.contacts.lenght !== this.state.contacts.length)
+      localStorage.setItem('contact', JSON.stringify(this.state.contacts));
   };
 
   formSubmitHandler = ({ name, number }) => {
